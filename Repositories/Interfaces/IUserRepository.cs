ab061dd1306adfe2380d6d46505c517d0cfca4ac
higorcsamtos <higorcsantos18@gmail.com>
2023-02-25 18:20:04 -0300

    creating users repository

--- Repositories/Interfaces/IUserRepository.cs
+++ Repositories/Interfaces/IUserRepository.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using firstApi.models;
 
 namespace firstApi.Repositories.Interfaces
 {
   public interface IUserRepository
   {
-
+    Task<List<UsersModel>> GetAllUsers();
+    Task<UsersModel> GetUserById(int id);
+    Task<UsersModel> CreateUser(UsersModel user);
+    Task<UsersModel> UpdateUser(UsersModel user);
+    Task<bool> Delete(int id);
   }
 }
